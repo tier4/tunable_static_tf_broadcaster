2ec443c79eec31c51bc04e53b7ec810275229717
Hayato Mizushima <hayato.mizushima@tier4.jp>
2019-10-18 14:09:54 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Tunable static tf broadcaster
+dynamic_reconfigureを用いて、GUIでtf2のxyzrpyを変更してbroadcastするノード
+
+![デモ](./demo.gif)
+
+## 動作
+以下を参考にxyzrpyをdynamic_reconfigureでいじれるようにしている
+- http://wiki.ros.org/tf2/Tutorials/Writing%20a%20tf2%20static%20broadcaster%20%28Python%29
+
+## quick start
+```
+roslaunch tunable_static_tf_broadcaster sample.launch
+```
+
+## tfのフレームの増やし方
+tunable_static_tf_broadcaster_nodeを複数立ち上げることでフレームを増やせます。
+
+1. sample.launchをコピーして適当な名前をつける
+2. 以下のノードのブロックを追加していく
+
+```
+  <!-- 追加するノード -->
+  <node name="ノード名" pkg="tunable_static_tf_broadcaster" type="tunable_static_tf_broadcaster_node.py" >
+    <param name="rate" value="10.0" type="double" /> <!-- tfの更新レート[Hz]-->
+    <param name="header_frame" value="親フレームのID" type="string" /> <!-- tfのヘッダフレームID -->
+    <param name="child_frame" value="子のフレームID" type="string" /> <!-- tfの子フレームID-->
+  </node>
+```
+
+## dynamic_reconfigureで設定したパラメータをロードする方法
+1. dynamic_reconfigureで適切なパラメータを設定し、GUIからyamlを保存する
+
+![パラメータ保存](./save_param.png)
+
+2. 保存したyamlをlaunchで起動時にロードするように設定する。以下のような行を追加する
+
+```
+<node name="load_ロードする設定名" pkg="dynamic_reconfigure" type="dynparam" args="load dynamic_reconfigureの名前 $(find tunable_static_tf_broadcaster)/params/パラメータファイル.yaml" />
+```
---
+++ scripts/tunable_static_tf_broadcaster_node.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy
+import tf
+import tf2_ros
+import geometry_msgs.msg
+from dynamic_reconfigure.server import Server
+from tunable_static_tf_broadcaster.cfg import TfConfig
+
+class TunableStaticTFBroadcaster:
+    def __init__(self):
+        self.__pub_rate = rospy.get_param('~rate', 10) #Hz
+        self.__rate = rospy.Rate(self.__pub_rate)
+        self.__tf_broadcaster = tf2_ros.StaticTransformBroadcaster()
+        self.__static_transformStamped = geometry_msgs.msg.TransformStamped()
+        self.__static_transformStamped.header.frame_id = rospy.get_param('~header_frame', "world")
+        self.__static_transformStamped.child_frame_id = rospy.get_param('~child_frame', "base_link")
+        self.__static_transformStamped.transform.translation.x = 0.1
+        self.__static_transformStamped.transform.translation.y = 0.1
+        self.__static_transformStamped.transform.translation.z = 0.1
+        self.__tf_roll = 0.0
+        self.__tf_pitch = 0.0
+        self.__tf_yaw = 0.0
+        self.__tf_quaternion = tf.transformations.quaternion_from_euler(self.__tf_roll, self.__tf_pitch, self.__tf_yaw)
+        self.__static_transformStamped.transform.rotation.x = self.__tf_quaternion[0]
+        self.__static_transformStamped.transform.rotation.y = self.__tf_quaternion[1]
+        self.__static_transformStamped.transform.rotation.z = self.__tf_quaternion[2]
+        self.__static_transformStamped.transform.rotation.w = self.__tf_quaternion[3]
+        self.__srv = Server(TfConfig, self.__reconfigure_callback)
+
+    def __reconfigure_callback(self, config, level):
+        self.__static_transformStamped.transform.translation.x = config.tf_x
+        self.__static_transformStamped.transform.translation.y = config.tf_y
+        self.__static_transformStamped.transform.translation.z = config.tf_z
+        self.__tf_roll = config.tf_roll
+        self.__tf_pitch = config.tf_pitch
+        self.__tf_yaw = config.tf_yaw
+        self.__tf_quaternion = tf.transformations.quaternion_from_euler(self.__tf_roll, self.__tf_pitch, self.__tf_yaw)
+        self.__static_transformStamped.transform.rotation.x = self.__tf_quaternion[0]
+        self.__static_transformStamped.transform.rotation.y = self.__tf_quaternion[1]
+        self.__static_transformStamped.transform.rotation.z = self.__tf_quaternion[2]
+        self.__static_transformStamped.transform.rotation.w = self.__tf_quaternion[3]
+        return config
+
+    def run(self):
+        while not rospy.is_shutdown():
+            self.__static_transformStamped.header.stamp = rospy.Time.now()
+            self.__tf_broadcaster.sendTransform(self.__static_transformStamped)
+            self.__rate.sleep()
+
+def main():
+    rospy.init_node('tunable_static_tf_broadcaster')
+    broadcaster = TunableStaticTFBroadcaster()
+    broadcaster.run()
+
+if __name__ == '__main__':
+    main()
